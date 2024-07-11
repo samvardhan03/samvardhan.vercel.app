2a96f9e315566eb475d7e53e7290a97e14837b1c
Samvardhan Singh <104402477+samvardhan03@users.noreply.github.com>
2024-07-11 21:37:08 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -24,8 +24,8 @@ const greeting = {
   nickname: "",
   subTitle:
     "I am an engineering student due to graduate in 2025,proficient in web development, neural networks and Python,I am Eager to continue pushing boundaries and making meaningful contributions in the tech industry.",
-  resumeLink:
-    "https://drive.google.com/file/d/1-ojFCaei3RfHYemdR_nnoUWehCz-TkAv/view",
+  resumeLink: 
+    "https://drive.google.com/file/d/1t55LX7TNGjbUjjfut0EM4UkYWyDGF9iY/view",
   portfolio_repository: "https://github.com/samvardhan03",
   githubProfile: "https://github.com/samvardhan03",
 };
